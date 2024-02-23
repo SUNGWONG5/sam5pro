c55de6d861c66b6c3aaf2373e608b159043cad83
SUNG5 <sungwong555@gmail.com>
2024-02-23 12:00:41 +0800

    update eslint

--- pages/index.js
+++ pages/index.js
@@ -5,23 +5,13 @@ const Home = () => (
   <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
-
-    <div className="relative">
-      <About />
-      <div className="gradient-03 z-0" />
-      <Explore />
-    </div>
-    <div className="relative">
-      <GetStarted />
-      <div className="gradient-04 z-0" />
-      <WhatsNew />
-    </div>
+    <About />
+    <Insights />
+    <Explore />
+    <GetStarted />
+    <WhatsNew />
     <World />
-    <div className="relative">
-        <Insights />
-        <div className="gradient-04 z-0" />
-      <Feedback />
-    </div>
+    <Feedback />
     <Footer />
   </div>
 );
